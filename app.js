67374904301621225b9fae99f87b88b411256382
bochiedev <bochiegfx@gmail.com>
2023-05-02 18:15:50 +0300

    edit styling and js for the image view after response

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// define my variables
 const API_KEY = "";
 const input = document.querySelector("input");
-const submitButton = document.querySelector("#submit-btn");
+const submitButton = document.querySelector(".btn");
+const imageSection = document.querySelector(".images-section");
+
 const url = "https://api.openai.com/v1/images/generations";
 
+// function to make a call to openai and get images
 const getImages = async () => {
+  // define options that will be used in fetch
   let options = {
     method: "POST",
     headers: {
@@ -18,13 +23,26 @@ const getImages = async () => {
   };
 
   try {
+    // make a call to openai using fetch
     const response = await fetch(url, options);
 
+    // get the response
     const data = await response.json();
-    console.log(data);
+
+    // add images from response to the view
+    data?.data.forEach((image) => {
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("image-container");
+      const imageElement = document.createElement("img");
+      imageElement.setAttribute(src, image.url);
+      imageContainer.append(imageElement);
+      imageSection.append(imageContainer);
+    });
   } catch (error) {
+    // log the error if any
     console.error(error);
   }
 };
 
+// Listen for the generate button click and run get images function
 submitButton.addEventListener("click", getImages);
